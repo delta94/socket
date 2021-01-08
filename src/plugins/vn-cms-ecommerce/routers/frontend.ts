e8def0966cd984f648cdc6cd5758633dfac69199
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2021-01-08 23:52:53 +0700

    Update

--- src/plugins/vn-cms-ecommerce/routers/frontend.ts
+++ src/plugins/vn-cms-ecommerce/routers/frontend.ts
@@ -3,15 +3,30 @@ import { add_router } from "hooks/routerhook";
 import { TableName } from "..";
 
 add_router('/product', async (req, res) => {
+    let { page, limit, sort }: any = req.query;
+    if (!page) page = 1;
+    if (!limit) limit = 15;
+    if (!sort) sort = '_id.-1'
 
-    let query: any = {}
+    sort = sort.split('.')
 
-    
+    sort = {
+        [sort[0]]: parseInt(sort[1])
+    }
+
+    let query: any = {
+        page, limit, sort
+    }
+
+
     let queryField = getModel(TableName.Product).getQueryFilter(req.query);
-    
-    if(Object.keys(queryField).length > 0){
+
+    if (Object.keys(queryField).length > 0) {
         query.match = queryField
     }
+
+
+
 
     console.log(query)
 
@@ -21,10 +36,6 @@ add_router('/product', async (req, res) => {
 
 
 add_router('/categories', async (req, res) => {
-
-
-    
-
     let { data } = await getModel(TableName.Category).findMany({ limit: 100 });
     res.json(data)
 })
