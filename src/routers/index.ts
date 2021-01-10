2a39e6740b0a9109ebcd588a490b039747d638ec
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2021-01-11 00:24:05 +0700

    Update

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,13 +1,27 @@
 import { getAllModel } from "core/Model";
+import { getModel } from "core/model/MongoDB";
 import { add_router } from "hooks/routerhook";
 add_router('test', (req, res) => {
     let models = getAllModel();
     console.log(models)
     let result = {};
-    for(let i in models){
+    for (let i in models) {
         result[i] = {
             _fields: models[i]._fields
         }
     }
     res.json(result)
+})
+
+add_router('/login', async (req, res) => {
+
+    let { username, password } = req.body
+
+    let { data } = await getModel('user').findOne({ match: { username, password } });
+
+    if (!data) {
+        return res.json(data)
+    }
+
+    return res.json({ error: 'Username hoặc passowrd không đúng' });
 })
